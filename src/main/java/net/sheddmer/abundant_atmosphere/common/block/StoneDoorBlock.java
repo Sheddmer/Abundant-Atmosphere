998adc6abfc551269e1b7141316ea262bd068dc1
Sheddmer <69223831+Sheddmer@users.noreply.github.com>
2025-08-28 13:58:20 -0700

    exportstuff? - Sheddmer

--- src/main/java/net/sheddmer/abundant_atmosphere/common/block/StoneDoorBlock.java
+++ src/main/java/net/sheddmer/abundant_atmosphere/common/block/StoneDoorBlock.java
@@ -3,19 +3,20 @@ package net.sheddmer.abundant_atmosphere.common.block;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
-import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DoorBlock;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.*;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
 import net.sheddmer.abundant_atmosphere.common.init.AASounds;
 
 import javax.annotation.Nullable;
@@ -43,6 +44,23 @@ public class StoneDoorBlock extends DoorBlock {
         }
     }
 
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        BlockPos blockpos = context.getClickedPos();
+        Level level = context.getLevel();
+        if (blockpos.getY() < level.getMaxBuildHeight() - 1 && level.getBlockState(blockpos.above()).canBeReplaced(context)) {
+            boolean flag = level.hasNeighborSignal(blockpos) || level.hasNeighborSignal(blockpos.above());
+            return this.defaultBlockState()
+                    .setValue(FACING, context.getHorizontalDirection())
+                    .setValue(HINGE, this.getHinge(context))
+                    .setValue(POWERED, Boolean.valueOf(flag))
+                    .setValue(HALF, DoubleBlockHalf.LOWER);
+        } else {
+            return null;
+        }
+    }
+
     @Override
     protected void neighborChanged(BlockState state, Level level, BlockPos pos, Block block, BlockPos fromPos, boolean isMoving) {
         boolean flag = level.hasNeighborSignal(pos) || level.hasNeighborSignal(pos.relative(state.getValue(HALF) == DoubleBlockHalf.LOWER ? Direction.UP : Direction.DOWN));
@@ -60,6 +78,45 @@ public class StoneDoorBlock extends DoorBlock {
     protected void playLockSound(@Nullable Player player, Level level, BlockPos pos, boolean isOpened) {
         level.playSound(player, pos, isOpened ? AASounds.STONE_DOOR_LOCK.get() : AASounds.STONE_DOOR_UNLOCK.get(), SoundSource.BLOCKS, 1.0F, level.getRandom().nextFloat() * 0.1F + 0.9F);
         level.gameEvent(player, isOpened ? GameEvent.BLOCK_ACTIVATE : GameEvent.BLOCK_DEACTIVATE, pos);
+    }
+
+    private DoorHingeSide getHinge(BlockPlaceContext context) {
+        BlockGetter blockgetter = context.getLevel();
+        BlockPos blockpos = context.getClickedPos();
+        Direction direction = context.getHorizontalDirection();
+        BlockPos blockpos1 = blockpos.above();
+        Direction direction1 = direction.getCounterClockWise();
+        BlockPos blockpos2 = blockpos.relative(direction1);
+        BlockState blockstate = blockgetter.getBlockState(blockpos2);
+        BlockPos blockpos3 = blockpos1.relative(direction1);
+        BlockState blockstate1 = blockgetter.getBlockState(blockpos3);
+        Direction direction2 = direction.getClockWise();
+        BlockPos blockpos4 = blockpos.relative(direction2);
+        BlockState blockstate2 = blockgetter.getBlockState(blockpos4);
+        BlockPos blockpos5 = blockpos1.relative(direction2);
+        BlockState blockstate3 = blockgetter.getBlockState(blockpos5);
+        int i = (blockstate.isCollisionShapeFullBlock(blockgetter, blockpos2) ? -1 : 0)
+                + (blockstate1.isCollisionShapeFullBlock(blockgetter, blockpos3) ? -1 : 0)
+                + (blockstate2.isCollisionShapeFullBlock(blockgetter, blockpos4) ? 1 : 0)
+                + (blockstate3.isCollisionShapeFullBlock(blockgetter, blockpos5) ? 1 : 0);
+        boolean flag = blockstate.getBlock() instanceof DoorBlock && blockstate.getValue(HALF) == DoubleBlockHalf.LOWER;
+        boolean flag1 = blockstate2.getBlock() instanceof DoorBlock && blockstate2.getValue(HALF) == DoubleBlockHalf.LOWER;
+        if ((!flag || flag1) && i <= 0) {
+            if ((!flag1 || flag) && i >= 0) {
+                int j = direction.getStepX();
+                int k = direction.getStepZ();
+                Vec3 vec3 = context.getClickLocation();
+                double d0 = vec3.x - (double)blockpos.getX();
+                double d1 = vec3.z - (double)blockpos.getZ();
+                return (j >= 0 || !(d1 < 0.5)) && (j <= 0 || !(d1 > 0.5)) && (k >= 0 || !(d0 > 0.5)) && (k <= 0 || !(d0 < 0.5))
+                        ? DoorHingeSide.LEFT
+                        : DoorHingeSide.RIGHT;
+            } else {
+                return DoorHingeSide.LEFT;
+            }
+        } else {
+            return DoorHingeSide.RIGHT;
+        }
     }
 
     public BlockSetType type() {
--- src/main/java/net/sheddmer/abundant_atmosphere/common/datamap/AABlockInteration.java
+++
@@ -1,9 +0,0 @@
-package net.sheddmer.abundant_atmosphere.common.datamap;
-
-import com.mojang.serialization.Codec;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.level.block.Block;
-
-public record AABlockInteration(Block block) {
-
-}
